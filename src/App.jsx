924785c3cad0c1a74d95bee88644e6931278a5b9
shivarawart <shivarawat076agmailcom>
2025-05-13 18:45:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    console.log(import.meta.env.VITE_APPWRITE_uRL);
+  return (
+    <div>
+      <h3>hello</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/config/conf.js
@@ -0,0 +1,11 @@
+const cong = {
+    appwrite:String(import.meta.env.VITE_APPWRITE_uRL),
+
+    projectId:String(import.meta.env. VITE_APPWRITE_PROJECT_ID),
+
+    databaseId:String(import.meta.env.VITE_APPWRITE_DATABASE),
+    collectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    apiKey:String(import.meta.env.VITE_APPWRITE_BACCKET),
+}
+
+export default conf
